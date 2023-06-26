827bfb5efa918a7fa5419c2f41fc29d1a749f3e0
Doug A <douganderson444@gmail.com>
2023-06-26 16:27:27 -0300

    listen after tick

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,6 +23,7 @@
 	 * @type {Node}
 	 */
 	let parentDiv;
+	let mod;
 
 	onMount(async () => {
 		// Set up a broadcast channel to listen for updates from the Blob URLs
@@ -43,20 +44,27 @@
 			files: [[importName, importables]]
 		};
 
-		let mod = await wasmComponentBytesToESModule({ wasmBytes, imprt: imports });
+		mod = await wasmComponentBytesToESModule({ wasmBytes, imprt: imports });
 
 		// @ts-ignore
 		whatSayYou = mod.render('World');
 		window.render = mod.render; // expose render function to blob URLs
-		await tick(); // wait for the DOM to be updated with the new Elements
+		// await tick(); // wait for the DOM to be updated with the new Elements
 
 		// Listen for messages from the Blob URLs created in wasmComponentBytesToESModule
 		bc.onmessage = (event) => {
 			whatSayYou = event.data;
 		};
-
-		mod.listen(); // listen for events to update the mod state
 	});
+
+	// need to apply listeners very time the DOM gets new render!
+	$: if (whatSayYou && mod)
+		(async () => {
+			// wait for the DOM to reflect the updates first
+			await tick();
+			mod.listen();
+			// put focus back on thelast focused element
+		})();
 </script>
 
 <div bind:this={parentDiv}>
