e846bf9265ead23d37053909e61513eede147433
Doug Anderson444 <douganderson444@gmail.com>
2023-12-07 06:02:40 -0400

    export example addeventlistener

---
+++ src/lib/imports.js
@@ -0,0 +1,16 @@
+import { LISTENER_UPDATES } from './index.js';
+
+/**
+ * @param {string} ty - type of event to listen for
+ * @param {function} handler - function to run when event is triggered
+ */
+export function addeventlistener({ selector, ty, value }) {
+	const bc = new BroadcastChannel(LISTENER_UPDATES);
+	let elem = document.querySelector(selector);
+	document.querySelector(selector).addEventListener(ty, (e) => {
+		// This code gets bundled into the main bundle, but there's no way for us
+		// to reference `render` directly. But we can access the window object
+		// which ensures we are using the same function. , we post a message to the parent window
+		bc.postMessage(window.wurbo.render(e.target.value));
+	});
+}
