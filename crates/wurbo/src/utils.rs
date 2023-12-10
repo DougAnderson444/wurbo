3afcac29ddd3110fb162a6464b34cb0e2b9d45e8
Doug Anderson444 <douganderson444@gmail.com>
2023-12-10 18:55:15 -0400

    alphs only

--- crates/wurbo/src/utils.rs
+++ crates/wurbo/src/utils.rs
@@ -2,9 +2,9 @@ use nanoid::nanoid;
 
 /// CSS Selector IDs need to start with a letter, so we can't use the default nanoid alphabet.
 /// We'll use this custom one instead.
-const ALPHABET: [char; 28] = [
+const ALPHABET: [char; 26] = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-    't', 'u', 'v', 'w', 'x', 'y', 'z', '_', '-',
+    't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
 /// Generate a random ID for a CSS selector
