cf8886d69756b5b6ef3536bfebb662c855099319
Doug Anderson444 <douganderson444@gmail.com>
2024-01-28 16:21:02 -0400

    export direct names
    util could conflict with others' utils crates

--- crates/wurbo/src/lib.rs
+++ crates/wurbo/src/lib.rs
@@ -8,7 +8,7 @@ pub mod utils;
 
 /// Prelude to ensure all users have the required deps
 pub mod prelude {
-    pub use crate::utils;
+    pub use crate::utils::{rand_id, variant_string, Base64JSON};
     pub use lazy_static::lazy_static;
     pub use minijinja::value::{Object, StructObject, Value};
     pub use minijinja::Environment;
