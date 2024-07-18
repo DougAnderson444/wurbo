9c109c6c1e490f4f072f7cde0ce92c3d5e8b5206
Doug Anderson444 <douganderson444@gmail.com>
2024-07-18 08:35:15 -0300

    update for new WurboComponent

--- src/examples/vowels/Vowels.svelte
+++ src/examples/vowels/Vowels.svelte
@@ -7,11 +7,28 @@
 
 	let name = 'demo:vowels';
 
+	let value = "Let's count vowels using templates for Inputs and Outputs!";
+
 	let data = {
-		page: { title: "Let's count vowels using templates for Inputs and Outputs!" },
-		input: { placeholder: "Input the word with vowels it's here" },
-		output: { value: 'vowels' }
+		tag: 'all-content',
+		val: {
+			page: { title: value },
+			input: { placeholder: "Input the word with vowels it's here" },
+			output: { value: 'vowels' }
+		}
 	};
+
+	// On update, send the new data to the component
+	function handleUpdate() {
+		data = {
+			tag: 'phrase',
+			val: value
+		};
+	}
 </script>
 
+<!-- A reactive Svelte code that allows the user here to edit the page.title to show Wurbo's reactivity to changing inputs -->
+<p>Wurbo changes reactively to the input below:</p>
+<input type="text" bind:value /><button on:click={handleUpdate}>Update</button>
+{JSON.stringify(data)}
 <WurboComponent {name} {wasmURL} {data} />
